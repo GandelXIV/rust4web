19d9b147b49419a760b0fd34232bf88e3f88241a
Filip Chovanec <filip@chovanec.com>
2024-02-03 16:17:49 +0100

    Migrate nginx to dev docker-compose

--- proxy.Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM nginx:1.25-alpine
-
-COPY static /srv/static
-
-COPY nginx.conf /etc/nginx/nginx.conf
-
-EXPOSE 7000
--- proxy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-sudo docker build -t tranx-proxy -f proxy.Dockerfile . 
-sudo docker run --net=host -it -p 7000:7000 tranx-proxy
-
