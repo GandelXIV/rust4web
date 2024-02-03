5466d95ce2a804971060a4d951ed1a62c1e3f0f9
Filip Chovanec <filip@chovanec.com>
2024-02-03 17:21:59 +0100

    Remove redundante model field

--- app/model.rs
+++ app/model.rs
@@ -1,16 +1,12 @@
 use std::sync::RwLock;
 
 pub struct Model {
-    pub score: RwLock<usize>,
     comments: RwLock<Vec<String>>,
 }
-
-const START_SCORE: usize = 0;
 
 impl Model {
     pub fn new() -> Self {
         Self {
-            score: RwLock::new(START_SCORE),
             comments: RwLock::new(vec!["Amazing website!".to_string()]),
         }
     }
@@ -21,18 +17,5 @@ impl Model {
 
     pub fn new_comment(&self, text: String) {
         self.comments.write().unwrap().push(text);
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn scoring() {
-        let model = Model::new();
-        assert_eq!(model.get_score(), START_SCORE);
-        model.increment_score();
-        assert_eq!(model.get_score(), START_SCORE + 1);
     }
 }
