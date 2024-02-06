76eafe2d0320cbcb7f80e6f9a349b5b2ac440d65
Filip Chovanec <filip@chovanec.com>
2024-02-06 11:20:23 +0100

    Integrate htmx-reset

---
+++ static/reset.js
@@ -0,0 +1,40 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+/*
+Source: http://www.github.com/GandelXIV/htmx-reset
+*/
+
+console.log("Loaded htmx-reset extension");
+
+function resetEventTrigger(event) {
+      const element = event.detail.requestConfig.elt;
+      element.reset();
+}
+
+htmx.defineExtension("reset-always", {
+  onEvent: function(name, event) {
+    if (name == "htmx:afterRequest") {
+      resetEventTrigger(event);
+    }
+  }
+});
+
+htmx.defineExtension("reset-success", {
+  onEvent: function(name, event) {
+    if (name == "htmx:afterRequest" && event.detail.successful) {
+      resetEventTrigger(event);
+    }
+  }
+})
+
+
+htmx.defineExtension("reset-failure", {
+  onEvent: function(name, event) {
+    if (name == "htmx:afterRequest" && !event.detail.successful) {
+      resetEventTrigger(event);
+    }
+  }
+})
